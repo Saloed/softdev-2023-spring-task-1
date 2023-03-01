4586b9a76b5ec32687df36208a02b46e32cc8430
MirazE1 <mirazelyubaev@gmail.com>
2023-03-01 16:25:58 +0300

    Елюбаев М.С., "Вариант 15 -- поле для крестиков-ноликов", гр.3530901/20003

--- task/src/main/java/task1/TicTacToe.java
+++ task/src/main/java/task1/TicTacToe.java
@@ -2,72 +2,84 @@ package task1;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 public class TicTacToe {
     private final int a;
-    private static String[][] field;
+    private final Marker[][] field;
+    public enum Marker {
+        EMPTY,
+        X,
+        O
+    }
     public TicTacToe(int a) {
-        if (a > 0) {
+        if (a <= 0) throw new IllegalArgumentException();
         this.a = a;
-        field = new String[a][a];
+        field = new Marker[a][a];
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                field[i][j] = Marker.EMPTY;
+            }
+        }
+    }
+    public void addX(int x, int y) {
+        if (x <= a && y <= a && x >= 0 && y >= 0) {
+            if (field[x][y] == Marker.EMPTY) field[x][y] = Marker.X;
+            else throw new IllegalArgumentException();
         } else throw new IllegalArgumentException();
     }
-    public void add(int x, int y, String p){
-        if (field[x][y] == null && (p == "X" || p == "O")) {
-            if (x <= a && y <= a && x >= 0 && y >= 0) {
-                field[x][y] = p;
-            } else throw new ArrayIndexOutOfBoundsException();
+    public void addO(int x, int y) {
+        if (x <= a && y <= a && x >= 0 && y >= 0) {
+            if (field[x][y] == Marker.EMPTY) field[x][y] = Marker.O;
+            else throw new IllegalArgumentException();
         } else throw new IllegalArgumentException();
     }
     public void remove(int x, int y){
-        if (x <= a && y <= a && x >= 0 && y >= 0) field[x][y] = null;
+        if (x <= a && y <= a && x >= 0 && y >= 0) field[x][y] = Marker.EMPTY;
         else throw new IllegalArgumentException();
     }
-    public int length(String p) {
-        if (p == "X" || p == "O") {
-            int x = 0;
-            int y = 0;
-            int control = 0;
-            int[][] hor = new int[a][a];
-            int[][] ver = new int[a][a];
-            int[][] diaN = new int[a][a];
-            int[][] diaZ = new int[a][a];
-            ArrayList<Integer> list = new ArrayList<>();
-            list.add(-1);
-            for (int j = 0; j < a; j++) {
-                for (int i = 0; i < a; i++) {
-                    if (Objects.equals(field[i][j], p)) {
-                        if (control == 0) {
-                            x = i;
-                            y = j;
-                            list.add(0);
-                            control = 1;
+    public int length(Marker p) {
+        int x = 0;
+        int y = 0;
+        boolean control = false;
+        int[][] hor = new int[a][a];
+        int[][] ver = new int[a][a];
+        int[][] diaN = new int[a][a];
+        int[][] diaZ = new int[a][a];
+        List<Integer> list = new ArrayList<>();
+        list.add(-1);
+        for (int j = 0; j < a; j++) {
+            for (int i = 0; i < a; i++) {
+                if (Objects.equals(field[i][j], p)) {
+                    if (!control) {
+                        x = i;
+                        y = j;
+                        list.add(0);
+                        control = true;
+                    } else {
+                        if (i == (x + 1) && j == y) {
+                            hor[i][j] = hor[x][y] + 1;
+                            list.add(hor[i][j]);
+                        } else if (i == x && j == (y + 1)) {
+                            ver[i][j] = ver[x][y] + 1;
+                            list.add(ver[i][j]);
+                        } else if (i == (x - 1) && j == (y + 1)) {
+                            diaN[i][j] = diaN[x][y] + 1;
+                            list.add(diaN[i][j]);
+                        } else if (i == (x + 1) && j == (y + 1)) {
+                            diaZ[i][j] = diaZ[x][y] + 1;
+                            list.add(diaZ[i][j]);
                         } else {
-                            if (i == (x + 1) && j == y) {
-                                hor[i][j] = hor[x][y] + 1;
-                                list.add(hor[i][j]);
-                            } else if (i == x && j == (y + 1)) {
-                                ver[i][j] = ver[x][y] + 1;
-                                list.add(ver[i][j]);
-                            } else if (i == (x - 1) && j == (y + 1)) {
-                                diaN[i][j] = diaN[x][y] + 1;
-                                list.add(diaN[i][j]);
-                            } else if (i == (x + 1) && j == (y + 1)) {
-                                diaZ[i][j] = diaZ[x][y] + 1;
-                                list.add(diaZ[i][j]);
-                            } else {
-                                i = x;
-                                j = y;
-                                control = 0;
-                            }
+                            i = x;
+                            j = y;
+                            control = false;
                         }
                     }
                 }
             }
-            System.out.println(list);
-            return Collections.max(list) + 1;
-        } else throw new IllegalArgumentException();
+        }
+        return Collections.max(list) + 1;
     }
 
 }
--- task/src/test/java/task1/TicTacToeTest.java
+++ task/src/test/java/task1/TicTacToeTest.java
@@ -8,79 +8,53 @@ public class TicTacToeTest {
 
     @Test
     public void test1() {
-        TicTacToe ticTacToe ;
-        ticTacToe = new TicTacToe(3);
-        ticTacToe.add(1, 0, "X");
-        ticTacToe.add(2, 1, "X");
-        ticTacToe.add(1, 2, "X");
-        ticTacToe.add(0,0, "O");
+        TicTacToe ticTacToe = new TicTacToe(3);
+        ticTacToe.addX(1, 0);
+        ticTacToe.addX(2, 1);
+        ticTacToe.addX(1, 2);
+        ticTacToe.addO(0, 0);
         ticTacToe.remove(2, 1);
-        assertEquals(1, ticTacToe.length("X"));
+        assertEquals( 1, ticTacToe.length(TicTacToe.Marker.X) );
     }
 
     @Test
     public void test2() {
-        IllegalArgumentException thrown = assertThrows(
-                IllegalArgumentException.class,
-                () -> new TicTacToe(-4));
+        assertThrows( IllegalArgumentException.class, () -> new TicTacToe(-4) );
     }
     @Test
     public void test3() {
-        TicTacToe ticTacToe ;
-        ticTacToe = new TicTacToe(3);
-        ticTacToe.add(1, 0, "X");
-
-        IllegalArgumentException thrown = assertThrows(
-                IllegalArgumentException.class,
-                () ->
-                    ticTacToe.add(2, 1, "i"));
+        TicTacToe ticTacToe = new TicTacToe(3);
+        ticTacToe.addX(1, 0);
+        assertThrows( IllegalArgumentException.class, () -> ticTacToe.addO(1, 0) );
 
     }
 
     @Test
     public void test4() {
-        TicTacToe ticTacToe ;
-        ticTacToe = new TicTacToe(3);
-        ticTacToe.add(1, 0, "X");
+        TicTacToe ticTacToe = new TicTacToe(3);
+        ticTacToe.addX(1, 0);
 
-        ArrayIndexOutOfBoundsException thrown = assertThrows(
-                ArrayIndexOutOfBoundsException.class,
-                () ->
-                        ticTacToe.add(-2, 1, "X"));
+        assertThrows( IllegalArgumentException.class, () -> ticTacToe.addX(-2, 1) );
 
     }
+
     @Test
     public void test5() {
-        TicTacToe ticTacToe ;
-        ticTacToe = new TicTacToe(3);
-        ticTacToe.add(1, 0, "X");
-
-        IllegalArgumentException thrown = assertThrows(
-                IllegalArgumentException.class,
-                () ->
-                        ticTacToe.length("a"));
-
+        TicTacToe ticTacToe = new TicTacToe(8);
+        assertEquals( 0, ticTacToe.length(TicTacToe.Marker.X) );
     }
 
     @Test
     public void test6() {
-        TicTacToe ticTacToe ;
-        ticTacToe = new TicTacToe(8);
-        assertEquals(0, ticTacToe.length("X"));
-    }
-
-    @Test
-    public void test7() {
-        TicTacToe ticTacToe ;
-        ticTacToe = new TicTacToe(4);
-        ticTacToe.add(1, 0, "X");
-        ticTacToe.add(2, 0, "X");
-        ticTacToe.add(2, 1, "X");
-        ticTacToe.add(2, 2, "X");
-        ticTacToe.add(1, 2, "X");
-        ticTacToe.add(1, 3, "X");
-        ticTacToe.add(0,0, "O");
+        TicTacToe ticTacToe = new TicTacToe(4);
+        ticTacToe.addX(1, 0);
+        ticTacToe.addX(2, 0);
+        ticTacToe.addX(2, 1);
+        ticTacToe.addX(2, 2);
+        ticTacToe.addX(1, 2);
+        ticTacToe.addX(1, 3);
+        ticTacToe.addO(0, 0);
         ticTacToe.remove(1, 3);
-        assertEquals(3, ticTacToe.length("X"));
+        assertEquals( 3, ticTacToe.length(TicTacToe.Marker.X) );
     }
 }
