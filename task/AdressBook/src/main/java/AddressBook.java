fc91e4262228cc6d85d48214ef2580f79d9a5412
Discovery19 <sergeymukiy@gmail.com>
2023-03-11 15:42:00 +0300

    11.03 Мукий Сергей Адресная книга

--- task/AdressBook/src/main/java/AddressBook.java
+++ task/AdressBook/src/main/java/AddressBook.java
@@ -13,39 +13,83 @@ class Address {
         this.house = house;
         this.flat = flat;
     }
-
 }
 
 public class AddressBook {
     Map<String, Address> book = new HashMap<>();
-
+    Map<String, List<String>> streetBook = new HashMap<>();
+    Map<String, Map<Integer, List<String>>> houseBook = new HashMap<>();
     public void add(String name, Address adress) {
         book.put(name, adress);
+        if (streetBook.containsKey(adress.street)) streetBook.get(adress.street).add(name);
+        else {
+            List<String> nameList = new ArrayList<>();
+            nameList.add(name);
+            streetBook.put(adress.street, nameList);
+        }
+
+        if (houseBook.containsKey(adress.street)) {
+            if (houseBook.get(adress.street).containsKey(adress.house))
+                houseBook.get(adress.street).get(adress.house).add(name);
+            else {
+                List<String> list = new ArrayList<>();
+                list.add(name);
+                houseBook.get(adress.street).put(adress.house, list);
+            }
+        } else {
+            Map<Integer, List<String>> map = new HashMap<>();
+            List<String> list = new ArrayList<>();
+            list.add(name);
+            map.put(adress.house, list);
+            houseBook.put(adress.street, map);
+        }
     }
 
     public void remove(String name) {
+        String street = book.get(name).street;
+        int houseNum = book.get(name).house;
+        Map<Integer, List<String>> houseMap = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        list.add(name);
+        houseMap.put(houseNum, list);
         book.remove(name);
+        streetBook.remove(street, name);
+        houseBook.remove(street, houseMap);
     }
 
     public void replace(String name, Address adress) {
+        String oldAdress = book.get(name).street;
+        int oldHouse = book.get(name).house;
         book.replace(name, adress);
+        List<String> list = new ArrayList<>();
+        list.add(name);
+        Map<Integer, List<String>> houseMap = new HashMap<>();
+        houseMap.put(adress.house, list);
+        if (streetBook.containsKey(adress.street)) {
+            streetBook.remove(oldAdress, name);
+            streetBook.get(adress.street).add(name);
+        } else {
+            streetBook.remove(oldAdress, name);
+            streetBook.put(adress.street, list);
+        }
+        if (houseBook.containsKey(adress.street) && !houseBook.get(adress.street).containsKey(adress.house)) {
+            houseBook.remove(oldHouse, name);
+            houseBook.put(adress.street, houseMap);
+        } else if (!houseBook.containsKey(adress.street)) {
+            houseBook.remove(oldHouse, name);
+            houseBook.put(adress.street, houseMap);
+        }
     }
-
     public Address findAdress(String name) {
         return book.get(name);
     }
-
     public List<String> findStreet(String street) {
-        List<String> res = new ArrayList<>();
-        for (HashMap.Entry<String, Address> entry : book.entrySet()) {
-            if (entry.getValue().street.equals(street)) res.add(entry.getKey());
-        }
-        return res;
+        return streetBook.get(street);
     }
     public List<String> findStreetAndHouse(String street, int house) {
         List<String> res = new ArrayList<>();
-        for (HashMap.Entry<String, Address> entry : book.entrySet()) {
-            if (entry.getValue().street.equals(street) && entry.getValue().house==house) res.add(entry.getKey());
+        for (Map.Entry<String, Address> entry : book.entrySet()) {
+            if (entry.getValue().street.equals(street) && entry.getValue().house == house) res.add(entry.getKey());
         }
         return res;
     }
--- task/AdressBook/src/test/java/AddressTest.java
+++ task/AdressBook/src/test/java/AddressTest.java
@@ -1,42 +1,42 @@
 import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 public class AddressTest {
     @Test
     public void testAdressBook() {
-
-        AddressBook adressBook=new AddressBook();
-        Address adr=new Address("Первая",20,30);
+        AddressBook adressBook = new AddressBook();
+        Address adr = new Address("Первая", 20, 30);
         adressBook.add("Кузнецов", adr);
-        Address adr2=new Address("Первая",120,130);
+        Address adr2 = new Address("Первая", 120, 130);
         adressBook.add("Сидоров", adr2);
-        Address adr1=new Address("Вторая",40,770);
+        Address adr1 = new Address("Вторая", 40, 770);
         adressBook.add("Петров", adr1);
-
-        Address adr6=new Address("remove",70,70);
-        adressBook.remove("remove");
-
-        Address adr3=new Address("Вторая",10,50);
-        adressBook.replace("Кузнецов",adr3);
-
-
-        Address adr4=new Address("qwe",40,130);
+        Address adr6 = new Address("remove", 70, 70);
+        adressBook.add("RemoveMan",adr6);
+        adressBook.remove("RemoveMan");
+        Address adr3 = new Address("Вторая", 10, 50);
+        adressBook.replace("Кузнецов", adr3);
+        Address adr4 = new Address("qwe", 40, 130);
         adressBook.add("zxc", adr4);
-        Address adr5=new Address("qwe",40,770);
+        Address adr5 = new Address("qwe", 40, 770);
         adressBook.add("asd", adr5);
-        List<String> houseExp=new ArrayList<>();
+        List<String> houseExp = new ArrayList<>();
         houseExp.add("asd");
         houseExp.add("zxc");
-        assertEquals(houseExp, adressBook.findStreetAndHouse("qwe",40));
-
+        assertEquals(houseExp, adressBook.findStreetAndHouse("qwe", 40));
         assertEquals(adr1, adressBook.findAdress("Петров"));
-
-        adressBook.findStreet("Вторая");
-        List<String> res=new ArrayList<>();
-        res.add("Кузнецов");
-        res.add("Петров");
-        assertEquals(res,adressBook.findStreet("Вторая"));
-
+        Address find1 = new Address("find", 10, 11);
+        adressBook.add("New1", find1);
+        Address find2 = new Address("find", 11, 11);
+        adressBook.add("New2", find2);
+        List<String> res = new ArrayList<>();
+        res.add("New1");
+        res.add("New2");
+        adressBook.findStreet("find");
+        assertEquals(res, adressBook.findStreet("find"));
     }
 }
