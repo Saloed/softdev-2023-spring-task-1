545fb8b35db93c451bb31a271fb690ec57f25c53
KarzhenkovaVA <karzhenkova.va@edu.spbstu.ru>
2023-03-14 20:52:43 +0300

    directedGraph changes

--- task/src/main/java/task1/Edge.java
+++ task/src/main/java/task1/Edge.java
@@ -1,44 +1,14 @@
 package task1;
 
-public class Edge {
-
-    private String start;
-    private String end;
-    private int weight;
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public String getStart() {
-        return start;
-    }
-
-    public String getEnd() {
-        return end;
-    }
-
-    public void setWeight(int weight) {
-        this.weight = weight;
-    }
-
-    void setStart(String start) {
-        this.start = start;
-    }
-
-    void setEnd(String end) {
-        this.end = end;
-    }
+class Edge {
+    public String start;
+    public String end;
+    public int weight;
 
     public Edge(String start, String end, int weight) {
         this.start = start;
         this.end = end;
         this.weight = weight;
-    }
-
-    public Edge(String start, String end) {
-        this.start = start;
-        this.end = end;
     }
 
     @Override
@@ -58,5 +28,4 @@ public class Edge {
         if (end != edge.end) return false;
         return true;
     }
-
 }
