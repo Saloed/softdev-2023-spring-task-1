17e4090099183e2986cfbba37f66f7e1b05dfe9e
KarzhenkovaVA <karzhenkova.va@edu.spbstu.ru>
2023-03-14 18:50:28 +0300

    directedGraph changes

--- task/src/main/java/task1/DirectedGraph.java
+++ task/src/main/java/task1/DirectedGraph.java
@@ -1,112 +1,128 @@
 package task1;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DirectedGraph {
 
-    private final Set<String> vertices = new HashSet<>();
-    private final Set<Edge> edges = new HashSet<>();
-    private final Map<String, Set<Edge>> ins = new HashMap<>();
-    private final Map<String, Set<Edge>> outs = new HashMap<>();
+    static class Vertex {
+        public Set<String> ins = new HashSet<>();
+        public Set<String> outs = new HashSet<>();
+    }
+
+    public static class Edge1 {
+        public String start;
+        public String end;
+        public int weight;
+
+        public Edge1(String start, String end, int weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+    }
+
+    private final Map<String, Vertex> vertices = new HashMap<>();
+    private final Map<Edge, Integer> e = new HashMap<>();
 
     public boolean addVertex(String vertexToAdd) {
-        ins.put(vertexToAdd, new HashSet<>());
-        outs.put(vertexToAdd, new HashSet<>());
+        Vertex vertex = vertices.putIfAbsent(vertexToAdd, new Vertex());
 
-        return vertices.add(vertexToAdd);
+        return vertex == null;
     }
 
     public boolean addEdge(String start, String end, int weight) {
-        Edge edge = new Edge(start, end, weight);
+        Vertex startV = vertices.get(start);
+        Vertex endV = vertices.get(end);
 
-        if (!vertices.contains(start) || !vertices.contains(end)) return false;
-        if (edges.contains(edge)) return false;
+        if (startV == null || endV == null) return false;
         if (weight < 0) return false;
+        if (!startV.outs.add(end)) return false;
+        if (!endV.ins.add(start)) return false;
 
-        edges.add(edge);
-
-        ins.get(end).add(edge);
-        outs.get(start).add(edge);
+        e.put(new Edge(start, end), weight);
 
         return true;
     }
 
     public boolean removeVertex(String vertexToRemove) {
-        Set<Edge> edgesToRemove = new HashSet<>();
+        Vertex vertex = vertices.get(vertexToRemove);
 
-        for (Edge e: edges) {
-            if (e.getStart() == vertexToRemove || e.getEnd() == vertexToRemove) {
-                edgesToRemove.add(e);
-            }
+        if (vertex == null) return false;
+
+        for (String vName: vertex.ins) {
+            vertices.get(vertexToRemove).outs.remove(vertexToRemove);
+            e.remove(new Edge(vName, vertexToRemove));
         }
 
-        edges.removeAll(edgesToRemove);
+        for (String vName: vertex.outs) {
+            vertices.get(vertexToRemove).ins.remove(vertexToRemove);
+            e.remove(new Edge(vertexToRemove, vName));
+        }
 
-        ins.remove(vertexToRemove);
-        outs.remove(vertexToRemove);
-
-        return vertices.remove(vertexToRemove);
+        return true;
     }
 
     public boolean removeEdge(String start, String end) {
-        Edge edge = new Edge(start, end);
+        if (e.remove(new Edge(start, end)) == null) return false;
 
-        if (!edges.contains(edge)) return false;
-
-        edges.remove(edge);
-
-        ins.get(end).remove(edge);
-        outs.get(start).remove(edge);
+        vertices.get(end).ins.remove(start);
+        vertices.get(start).outs.remove(end);
 
         return true;
     }
 
     public boolean changeName(String nameToChange, String newName) {
-        if (vertices.contains(newName)) return false;
-        if (!vertices.remove(nameToChange)) return false;
+        Vertex vertex = vertices.get(nameToChange);
+        if (vertex == null) return false;
 
-        addVertex(newName);
+        for (String vName: vertex.ins) {
+            Set<String> outs = vertices.get(vName).outs;
+            outs.remove(nameToChange);
+            outs.add(newName);
 
-        for (Edge e: edges) {
-            if (e.getStart() == nameToChange) {
-                e.setStart(newName);
-                if (outs.containsKey(nameToChange)) {
-                    outs.put(newName, outs.get(nameToChange));
-                    outs.remove(nameToChange);
-                }
-            }
-            if (e.getEnd() == nameToChange) {
-                e.setEnd(newName);
-                if (ins.containsKey(nameToChange)) {
-                    ins.put(newName, ins.get(nameToChange));
-                    ins.remove(nameToChange);
-                }
-            }
+            Integer weight = e.remove(new Edge(vName, nameToChange));
+            e.put(new Edge(vName, newName), weight);
+        }
+
+        for (String vName: vertex.outs) {
+            Set<String> ins = vertices.get(vName).ins;
+            ins.remove(nameToChange);
+            ins.add(newName);
+
+            Integer weight = e.remove(new Edge(nameToChange, vName));
+            e.put(new Edge(newName, vName), weight);
         }
 
         return true;
     }
 
     public boolean changeWeight(String start, String end, int weight) {
-        Edge edge = new Edge(start, end);
-
-        if (!edges.contains(edge)) return false;
         if (weight < 0) return false;
-
-        edge.setWeight(weight);
+        if (e.replace(new Edge(start, end), weight) == null) return false;
 
         return true;
     }
 
-    public Set<Edge> getIns(String name) {
-        return ins.getOrDefault(name, new HashSet<>());
+    public Set<Edge1> getIns(String name) {
+        Set<Edge1> edges = new HashSet<>();
+        Vertex vertex = vertices.get(name);
+        if (vertex == null) return edges;
+
+        for (String v: vertex.ins) {
+            edges.add(new Edge1(v, name, e.get(new Edge(v, name))));
+        }
+        return edges;
     }
 
-    public Set<Edge> getOuts(String name) {
-        return outs.getOrDefault(name, new HashSet<>());
+    public Set<Edge1> getOuts(String name) {
+        Set<Edge1> edges = new HashSet<>();
+        Vertex vertex = vertices.get(name);
+        if (vertex == null) return edges;
+
+        for (String v: vertex.outs) {
+            edges.add(new Edge1(name, v, e.get(new Edge(name, v))));
+        }
+        return edges;
     }
 
 }
